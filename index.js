52dc08a3ef91f133269a100f0906c336cce25918
SuperMeerkat345 <116471191+SuperMeerkat345@users.noreply.github.com>
2024-02-02 11:28:09 -0500

    Update index.js
    updated to latest version, idk why it wasn't like this in the first place

--- index.js
+++ index.js
@@ -1,13 +1,69 @@
 //Start of code
 
-
-export function add(x, y){
-console.log(x + y);
-
+function print(input) {
+    console.log(input);
 }
 
-export function subtract(x, y){
-    console.log(x - y);
-    
+//start runtime functions
+
+function wholeTest(n) { 
+    let result = (n - Math. floor(n)) !== 0; 
+    return result;
 }
 
+function undefinedTest(n) {
+    let result = (n) !== undefined; 
+    return result;
+}
+
+function Counter(counting, countingInteval, startPoint, endPoint, printVals, runFunction, runFunctionInterval) {
+    this.countingInterval = countingInteval;
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+    this.counting = counting;
+    this.printVals = printVals;
+    this.runFunction = runFunction;
+    this.runFunctionInterval = runFunctionInterval;
+
+    this.count = startPoint;
+};
+
+Counter.prototype.start = function(){
+    while(this.counting && this.count < this.endPoint){
+        this.count += this.countingInterval;
+        if(this.printVals){
+            console.log(this.count);
+        }
+        if(undefinedTest(this.runFunction) && wholeTest(this.count/this.runFunctionInterval))       {
+            this.runFunction();
+        }
+    }            
+};
+
+function arrayIterate(array, runFunctionI, functionIntervalI, runFunctionJ, functionIntervalJ, printxy){
+    let x = 0;
+    let y = 0;
+    for(let i = 0; i < array; i++){
+        if(undefinedTest(runFunctionI) && undefinedTest(functionIntervalI)){
+            if(wholeTest(x/functionIntervalI)){
+                runFunctionI();
+            }
+            for(let j = 0; j < array[i]; j++){
+                if(undefinedTest(runFunctionI) && undefinedTest(functionIntervalI)){
+                    if(wholeTest(y/functionIntervalJ)){
+                        runFunctionJ();
+                        if(undefinedTest(printxy)){
+                            if(printxy == true){
+                                console.log(array[i][j]);
+                            }
+                        }
+                    }
+                }
+                y++;
+            }
+        }
+        x++;
+    }
+}
+
+module.exports = { print, Counter, wholeTest, undefinedTest, arrayIterate };
